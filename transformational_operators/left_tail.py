790f35dacaf7dd82050364499cb36ba19597c420
trung-vt <trung.vuthanh24@gmail.com>
2024-03-31 14:56:35 +0100

    Add `left_tail` transformational operator

--- transformational_operators/left_tail.py
+++ transformational_operators/left_tail.py
@@ -1,28 +1,29 @@
 import numpy as np
 import torch
 
-def right_tail(x, minimum = 0):
+def left_tail(x, maximum = 0):
+
     '''
-    NaN everything less than minimum, minimum should be constant.
-    This operator changes to NaN everything less than specified value.
+    NaN everything greater than maximum, maximum should be constant.
+    This operator changes to NaN everything greater than a specified value.
     
     Example:
     x                      = [0.1, 0.3, 0.5, 0.7, 0.9]
-    minimum                = 0.5
-    right_tail(x, minimum) = [nan, nan, 0.5, 0.7, 0.9]
+    maximum                = 0.5
+    left_tail(x, maximum)  = [0.1, 0.3, 0.5, nan, nan]
     
     Example taken from: https://platform.worldquantbrain.com/learn/data-and-operators/detailed-operator-descriptions#arc_tanx
-        right_tail(rank(volume), minimum = 0.5)
-            returns NaN, if rank(volume) < 0.5; else returns rank(volume).
-        Note: `volume` is a field in the "Price Volume Data for Equity" dataset. Type: Matrix.
+        left_tail(rank(close), maximum = 0.02)
+            returns NaN, if rank(close)> 0.02; else returns rank(close).
+        Note: `close` is a field in the "Price Volume Data for Equity" dataset. Type: Matrix.
             All datasets: https://platform.worldquantbrain.com/data/data-sets?delay=1&instrumentType=EQUITY&limit=20&offset=0&region=USA&universe=TOP3000
       
     Doctest:
     >>> x = torch.tensor([0.1, 0.3, 0.5, 0.7, 0.9])
-    >>> np.round(right_tail(x, minimum = 0.5), 1).tolist()
-    [nan, nan, 0.5, 0.7, 0.9]
+    >>> np.round(left_tail(x, maximum = 0.5), 1).tolist()
+    [0.1, 0.3, 0.5, nan, nan]
     '''
-    x[x < minimum] = float('nan')
+    x[x > maximum] = float('nan')
     return x.tolist()
 
     
